b71b6fe1d7c8003879522a2acd700e0fdcf3abf4
AngeLopez <angel@angelopez.ca>
2015-06-11 18:56:10 -0400

    Stuff works now

--- mars_rover.rb
+++ mars_rover.rb
@@ -5,60 +5,56 @@ class Rover
     @x = x
     @y = y
     @direction = direction
+    @@compass = ["N","E","S","W"]
   end
 
-  @@compass = ["N","E","S","W"]
-
-  DIRECTION_MATH = {
-    :N => [0, 1],
-    :E => [1, 0],
-    :S => [0, -1],
-    :W => [-1, 0]
-  }
-
-input.each_char{|c| c.read_instruction}
-
   def read_instruction(input)
-    if input == "L"
-      puts "turn(input)"
-    elsif input == "R"
-      puts "turn(input)"
-    elsif input == "M"
-      puts "move(input)"
+    input.each_char do |c|
+        if c == "L" || c == "R"
+          turn(c)
+        elsif c == "M"
+          move(c)
+        end
     end
   end
 
 
   def turn(command)
+    position = @@compass.index(@direction)
     if command == "L"
       @@compass.rotate!(-1)
+      @direction = @@compass[0]
     elsif command == "R"
       @@compass.rotate!
+      @direction = @@compass[0]
     end
-    puts @@compass[0]
   end
 
-  # def move(input)
-  #   Dx, Dy = DIRECTION_MATH[input]
-  #   # if input == "N"
-  #   # elsif input == "E"
-    # elsif input == "S"
-    # elsif input == "W"
-    # end
-  # end
 
-  def math
-    puts @@compassMath
+  def move(direction)
+    case @direction
+      when "N"
+        @y += 1
+      when "E"
+        @x += 1
+      when "S"
+        @y -= 1
+      when "W"
+        @x -= 1
+    end
   end
+
 
   def to_s
-    puts ""
-
+    puts "#{x}, #{y} and facing #{@direction}"
   end
 
 end
 
-one = Rover.new(4,6,"N")
-# one.turn("R")
-# one.move(@direction)
-one.read_instruction("R")
+# one = Rover.new(1,2,"N")
+# one.read_instruction("LMLMLMLMM")
+# puts one
+
+two = Rover.new(3,3,"E")
+two.read_instruction("MMRMMRMRRM")
+puts two
