ae72f6c1e6b87a16999eab15e9c2cc32f49252f3
AngeLopez <angel@angelopez.ca>
2015-06-11 13:31:30 -0400

    Problems completed, comments added

--- people.rb
+++ people.rb
@@ -1,22 +1,8 @@
 class Person
+  attr_accessor :name
 
   def initialize(name)
     @name = name
-  end
-
-  class Student < Person
-
-    def learn
-      puts "I get it!"
-    end
-  end
-
-
-  class Instructor < Person
-
-    def teach
-      puts "Everything in Ruby is an Object"
-    end
   end
 
   def greeting
@@ -24,11 +10,33 @@ class Person
   end
 end
 
-chris = Person.new("Chris")
-chris = Instructor.new
+class Student < Person
+    def learn
+      puts "I get it!"
+    end
+end
+
+class Instructor < Person
+    def teach
+      puts "Everything in Ruby is an Object"
+    end
+end
+
+# 6
+chris = Instructor.new("Chris")
+chris.greeting
+
+# 7
+cristina = Student.new("Cristina")
+cristina.greeting
+
+# 8
 chris.teach
-chris. greeting
+cristina.learn
 
-# cristina = Student.new("Cristina")
-# cristina.greeting
+# Call teach method on your student instance
+cristina.teach
 
+# What happens? Why doesn't that work?
+# When I do this, it creates a method error because there is no method
+# within the student class called teach.
